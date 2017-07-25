595796e997de483d9d3aee2c2e7151c7967b4f2c
JohnACastrillon <john.castrillon@nbcuni.com>
2017-07-25 10:24:10 -0400

    add eslint

--- angular/5/scripts/app.js
+++ angular/5/scripts/app.js
@@ -10,15 +10,27 @@ angular.module("todoListApp", [])
   $scope.learningNgChange = function() {
     console.log("An input changed");
   };
+
+  dataService.getTodos(function(response) {
+      console.log(response.data);
+      $scope.todos = response.data;
+  });
 })
 .service('dataService', function ($http) {
   this.helloConsole = function () {
     console.log('This is the hello console service!');
   }
 
-  this.getTodos = $http.get('mock/todos.json')
-                      .then(function (res) {
-                        console.log(res.data);
-                        return res.data;
-                      })
+  this.getTodos = function(callback) {
+                    $http.get('mock/todos.json')
+                    .then(callback);
+  }
+
+  this.deleteTodo = function(todo) {
+    console.log("The todo " + todo.name + " is deleted!");
+  }
+
+  this.saveTodo = function(todo) {
+    console.log("The todo " + todo.name + " is saved!");
+  }
 });
